ed0bc2e7f4f85afa8fc40b25f34fdfc8eff9cf54
musfique113 <musfique113@gmail.com>
2025-09-30 10:59:26 +0600

    feat: ✨ Add request, response and error logging in `NetworkExecutor`

--- lib/core/network/network_executor.dart
+++ lib/core/network/network_executor.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:developer' as developer;
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'network_exceptions.dart';
@@ -10,10 +12,14 @@ class NetworkExecutor {
   NetworkExecutor(this._client);
 
   Future<dynamic> get(String url, {Map<String, String>? headers}) async {
+    _logRequest('GET', url, headers: headers);
+
     try {
       final response = await _client.get(Uri.parse(url), headers: headers);
+      _logResponse('GET', url, response);
       return _handleResponse(response);
     } catch (e) {
+      _logError('GET', url, e);
       throw NetworkException('Failed to connect to the server');
     }
   }
@@ -23,14 +29,18 @@ class NetworkExecutor {
     Map<String, String>? headers,
     dynamic body,
   }) async {
+    _logRequest('POST', url, headers: headers, body: body);
+
     try {
       final response = await _client.post(
         Uri.parse(url),
         headers: headers,
         body: json.encode(body),
       );
+      _logResponse('POST', url, response);
       return _handleResponse(response);
     } catch (e) {
+      _logError('POST', url, e);
       throw NetworkException('Failed to connect to the server');
     }
   }
@@ -40,23 +50,31 @@ class NetworkExecutor {
     Map<String, String>? headers,
     dynamic body,
   }) async {
+    _logRequest('PUT', url, headers: headers, body: body);
+
     try {
       final response = await _client.put(
         Uri.parse(url),
         headers: headers,
         body: json.encode(body),
       );
+      _logResponse('PUT', url, response);
       return _handleResponse(response);
     } catch (e) {
+      _logError('PUT', url, e);
       throw NetworkException('Failed to connect to the server');
     }
   }
 
   Future<dynamic> delete(String url, {Map<String, String>? headers}) async {
+    _logRequest('DELETE', url, headers: headers);
+
     try {
       final response = await _client.delete(Uri.parse(url), headers: headers);
+      _logResponse('DELETE', url, response);
       return _handleResponse(response);
     } catch (e) {
+      _logError('DELETE', url, e);
       throw NetworkException('Failed to connect to the server');
     }
   }
@@ -81,6 +99,71 @@ class NetworkExecutor {
         throw NetworkException(
           'Received invalid status code: ${response.statusCode}',
         );
+    }
+  }
+
+  void _logRequest(
+    String method,
+    String url, {
+    Map<String, String>? headers,
+    dynamic body,
+  }) {
+    if (kDebugMode) {
+      final prettyJson = body != null
+          ? const JsonEncoder.withIndent('  ').convert(body)
+          : "None";
+
+      final logMessage =
+          '''
+      🌐 REQUEST
+      ---------------------------------
+      Method: $method
+      URL: $url
+      Headers: ${headers ?? "None"}
+      Body: $prettyJson
+      ---------------------------------
+      ''';
+      developer.log(logMessage, name: 'NetworkExecutor');
+    }
+  }
+
+  void _logResponse(String method, String url, http.Response response) {
+    if (kDebugMode) {
+      final prettyJson = const JsonEncoder.withIndent(
+        '  ',
+      ).convert(json.decode(response.body));
+
+      final logMessage =
+          '''
+      ✅ RESPONSE
+      ---------------------------------
+      Method: $method
+      URL: $url
+      Status Code: ${response.statusCode}
+      Headers: ${response.headers}
+      Body: $prettyJson
+      ---------------------------------
+      ''';
+      developer.log(logMessage, name: 'NetworkExecutor');
+    }
+  }
+
+  void _logError(String method, String url, dynamic error) {
+    if (kDebugMode) {
+      final logMessage =
+          '''
+      ❌ ERROR
+      ---------------------------------
+      Method: $method
+      URL: $url
+      Error: $error
+      ---------------------------------
+      ''';
+      developer.log(
+        logMessage,
+        name: 'NetworkExecutor',
+        error: error is Error ? error : null,
+      );
     }
   }
 }
--- lib/features/sentence/data/datasources/tatoeba_remote_datasource_impl.dart
+++ lib/features/sentence/data/datasources/tatoeba_remote_datasource_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:lingua/core/constants/app_constant.dart';
 import 'package:lingua/core/network/network_executor.dart';
 import 'package:lingua/features/sentence/data/models/sentence_model.dart';
@@ -8,15 +9,21 @@ class TatoebaRemoteDataSourceImpl implements TatoebaRemoteDataSource {
 
   TatoebaRemoteDataSourceImpl({required this.networkExecutor});
 
-  String _urlPath(int randomId) => '/unstable/sentences/$randomId';
+  String _urlPath(int randomId) => 'unstable/sentences/$randomId';
 
   @override
   Future<SentenceModel> fetchRandomSentence() async {
     final randomId = DateTime.now().millisecondsSinceEpoch % 10000000;
-    final response = await networkExecutor.get(
-      '${AppConstant.baseUrl}${_urlPath(randomId)}',
-    );
 
-    return SentenceModel.fromJson(response['data']);
+    try {
+      final response = await networkExecutor.get(
+        '${AppConstant.baseUrl}${_urlPath(randomId)}',
+      );
+
+      return SentenceModel.fromJson(response['data']);
+    } catch (e, stack) {
+      debugPrintStack(label: e.toString(), stackTrace: stack);
+      throw Exception('Error fetching random sentence: $e');
+    }
   }
 }
