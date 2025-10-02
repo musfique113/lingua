51db53bb10859f40f6c2e354be7e5bb9c10333fe
musfique113 <musfique113@gmail.com>
2025-10-02 17:26:10 +0600

    docs: :memo: Update README.md and remove debug banner

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         routes: AppRoutes.routes,
         theme: AppTheme.lightTheme(),
         darkTheme: AppTheme.darkTheme(),
+        debugShowCheckedModeBanner: false,
         builder: (context, child) {
           return BlocListener<ConnectivityBloc, ConnectivityState>(
             listenWhen: (previous, current) {
